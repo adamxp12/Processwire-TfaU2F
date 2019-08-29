11781e971d949dbe3b6ab0db4a55179f6066d02d
Adam Blunt <adamxp12@adamxp12.com>
2019-08-29 14:31:01 +0100

    Multiple Key Support

--- TfaU2F.info.php
+++ TfaU2F.info.php
@@ -2,7 +2,7 @@
 $info = array(
 	'title' => 'U2F two-factor authentication ',
 	'summary' => 'For two-factor authentication with security keys like Yubikey/Google Titan.',
-	'version' => 100,
+	'version' => 101,
 	'singular' => true,
 	'requires' => 'ProcessWire>=3.0.109'
 );
--- TfaU2F.js
+++ TfaU2F.js
@@ -1,8 +1,10 @@
+var TfaU2F_count = 1;
 function TfaU2F_addkey() {
     
     var appId = document.getElementById('TfaU2F_appId').value;
     var regdata = document.getElementById('TfaU2F_regdata');
     var challenge = JSON.parse("[" + document.getElementById('TfaU2F_challenge').value + "]");
+    
     u2f.register(appId, challenge, [], function(data) {
         console.log("Register callback", data);
                 if(data.errorCode && data.errorCode != 0) {
@@ -10,18 +12,15 @@ function TfaU2F_addkey() {
                     return;
                     
                 } else {
-                    /* 
-                     * This addition would in therory allow multiple keys to work.
-                     * But the is truncation happening somewhere in processwire so this is a waste XD
-                     */
                     if(regdata.value === "") {
                         regdata.value = JSON.stringify(data);
+                        document.getElementById('TfaU2F_msg').textContent = 'Security key added. You can add more now or save this page to finish setup';
                     } else {
                         regdata.value = regdata.value+", "+JSON.stringify(data);
-                    }
-                    
-                    document.getElementById('TfaU2F_msg').textContent = 'Security key added save this page to continue';
-                    
+                        document.getElementById('TfaU2F_msg').textContent = 'Security key '+TfaU2F_count+' added. You can add even more or save this page to finish setup';
+                    }    
+                    TfaU2F_count++;
+                    document.getElementById('TfaU2F_button').classList.remove('ui-state-active');                
                 }
             });
 }
