0b73d216e9baba45f29cd18b48fcd0ce1778c0dd
Adam Blunt <adamxp12@adamxp12.com>
2020-12-26 22:25:18 +0000

    Update TfaU2F.info.php

--- TfaU2F.info.php
+++ TfaU2F.info.php
@@ -2,7 +2,7 @@
 $info = array(
 	'title' => 'U2F two-factor authentication ',
 	'summary' => 'For two-factor authentication with security keys like Yubikey/Google Titan.',
-	'version' => 102,
+	'version' => 103,
 	'singular' => true,
 	'requires' => 'ProcessWire>=3.0.109'
 );
